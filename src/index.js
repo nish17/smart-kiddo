b2778b7dfc3f2bdcfd6d9802ae71872d30efa4d1
Nimesh <nimesh3536@gmail.com>
2018-01-09 16:57:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,114 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+const APP_ID = 'amzn1.ask.skill.d9a0c4ea-8034-4122-a37a-ad5158458c4d';
+
+const SKILL_NAME = 'New year Facts';
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = 'You can say tell me a new year fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+const facts = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '.'
+];
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
+    },
+    'pokemonFactsIntent': function () {
+        const factArr = facts;
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+        const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+        this.response.cardRenderer(SKILL_NAME, randomFact);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+    'dragonBallZIntent': function () {
+      const factArr = facts;
+      const factIndex = Math.floor(Math.random() * factArr.length);
+      const randomFact = factArr[factIndex];
+      const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+      this.response.cardRenderer(SKILL_NAME, randomFact);
+      this.response.speak(speechOutput);
+      this.emit(':responseReady');
+  },    
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
